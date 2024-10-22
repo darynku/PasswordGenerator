f9b2faebd260b9f6e7090b1724d45de1a18425c5
Stem Stem <stem@astanait.edu.kz>
2024-10-22 18:07:47 +0500

    #2 Kefteme

--- src/main/java/PasswordProject/PasswordProject/controller/contracts/PasswordRequest.java
+++ src/main/java/PasswordProject/PasswordProject/controller/contracts/PasswordRequest.java
@@ -3,10 +3,29 @@ package PasswordProject.PasswordProject.controller.contracts;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record PasswordRequest(int length) {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record PasswordRequest(
+        int length,
+        boolean useUpperCase,
+        boolean useLowerCase,
+        boolean useNumbers,
+        boolean useSymbols
+) {
 
     @JsonCreator
-    public PasswordRequest(@JsonProperty("length") int length) {
+    public PasswordRequest(
+            @JsonProperty("length") int length,
+            @JsonProperty("useUpperCase") boolean useUpperCase,
+            @JsonProperty("useLowerCase") boolean useLowerCase,
+            @JsonProperty("useNumbers") boolean useNumbers,
+            @JsonProperty("useSymbols") boolean useSymbols
+    ) {
         this.length = length;
+        this.useUpperCase = useUpperCase;
+        this.useLowerCase = useLowerCase;
+        this.useNumbers = useNumbers;
+        this.useSymbols = useSymbols;
     }
 }
